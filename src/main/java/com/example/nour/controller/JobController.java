6192a79df7e540d161ce62c76e2d029d491a6e5b
DavyUVD <desgarduria@gmail.com>
2018-12-19 22:40:10 +0100

    [feat] Job entity management : list and edition

--- src/main/java/com/example/nour/controller/JobController.java
+++ src/main/java/com/example/nour/controller/JobController.java
@@ -8,10 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.nour.model.Country;
 import com.example.nour.model.Job;
 import com.example.nour.repository.JobRepository;
 
@@ -21,6 +24,37 @@ public class JobController {
 	
 	@Autowired
 	private JobRepository jobRepository;
+	
+	@GetMapping(path="/all")
+	public String allJob(Model model) {
+		model.addAttribute("jobs", jobRepository.findAll());
+		return "jobs";
+	}
+	
+	@GetMapping(path="/edit/{id}")
+	public String edit(Model model,@PathVariable String id) {
+		Optional<Job> opt = jobRepository.findById(id);
+		
+		Job job = opt.get();
+		model.addAttribute("jobForm", job);
+		
+		return "jobEdit";
+	}
+	
+	@PostMapping(path="/update")
+	public String update(Model model, @ModelAttribute Job jo) {
+		
+		Optional<Job> opt =jobRepository.findById(jo.getJobId());
+		
+		Job job = opt.get();
+		job.setJobTitle(jo.getJobTitle());
+		job.setMaxSalary(jo.getMaxSalary());
+		job.setMinSalary(jo.getMinSalary());
+		
+		jobRepository.save(job);
+		
+		return "redirect:all";
+	}
 	
 	@GetMapping(path="/update/{id}/{name}")
 	public  @ResponseBody Job update(@PathVariable String id, @PathVariable String name){
@@ -39,16 +73,10 @@ public class JobController {
 		return jobRepository.findByMinSalaryGreaterThanOrderByMinSalaryDesc(min);
 	}
 	
-	@GetMapping(path="/all")
+	@GetMapping(path="/alljson")
 	public @ResponseBody List<Job> allJob(){
 		return (List<Job>) jobRepository.findAll();
 	}	
 	
-	@GetMapping(path="/tout")
-	public String allCountries() {
-		//model.addAttribute("countries", countryRepository.findAll());
-		
-		return "jobs.jsp";
-	}
 
 } 
