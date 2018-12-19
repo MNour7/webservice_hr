8e091a703506a1bb2cd0a163a95cca5e22905616
DavyUVD <desgarduria@gmail.com>
2018-12-19 22:41:03 +0100

    [clean] Country entity management : cleaned code

--- src/main/java/com/example/nour/controller/CountryController.java
+++ src/main/java/com/example/nour/controller/CountryController.java
@@ -2,7 +2,6 @@ package com.example.nour.controller;
 
 import java.util.Optional;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.example.nour.model.Country;
-import com.example.nour.model.CountryDTO;
 import com.example.nour.repository.CountryRepository;
 
 @Controller
@@ -21,18 +19,19 @@ public class CountryController {
 
 	@Autowired
 	private CountryRepository countryRepository;
-	private ModelMapper modelMapper;
+	
+	@GetMapping(path="/all")
+	public String allCountries(Model model) {
+		model.addAttribute("countries", countryRepository.findAll());
+		
+		return "countries";
+	}
 	
 	@GetMapping(path="/edit/{id}")
 	public String edit(Model model,@PathVariable String id) {
 		Optional<Country> opt = countryRepository.findById(id);
-		
-		Country country = opt.get();
-//		System.err.println("NAme : "+country.getCountryName());
-//		CountryDTO countryDTO =   modelMapper.map(country, CountryDTO.class);
-		
+		Country country = opt.get();	
 		model.addAttribute("countryForm", country);
-		
 		
 		return "countryEdit";
 	}
@@ -50,10 +49,5 @@ public class CountryController {
 		return "redirect:all";
 	}
 	
-	@GetMapping(path="/all")
-	public String allCountries(Model model) {
-		model.addAttribute("countries", countryRepository.findAll());
-		
-		return "countries";
-	}
+
 }
