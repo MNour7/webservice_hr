2b7f65a4a8f4ca0176f829e40d5b64e4c6a779e7
Kahina <kahinaouzia12@gmail.com>
2018-12-25 15:59:52 +0100

    jUnit job test

--- src/test/java/com/example/nour/NourDemoApplicationTests.java
+++ src/test/java/com/example/nour/NourDemoApplicationTests.java
@@ -1,17 +1,44 @@
 package com.example.nour;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+import org.springframework.test.context.junit4.SpringRunner;//I got the error in this line
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import com.example.nour.model.Job;
+import com.example.nour.repository.JobRepository;
 
-@RunWith(SpringRunner.class)
+//the test run before this error unknown..of import repo & springRunner class.. any clue
+@RunWith(SprigRunner.class)
+
 @SpringBootTest
 public class NourDemoApplicationTests {
 
+	
+	@Autowired
+	JobRepository jobRepository;
+	Job job;
+	
+	
+	
+	
 	@Test
 	public void contextLoads() {
+		
+		Optional<Job> op = jobRepository.findById("AC_ACCOUNT");
+		Job job = op.get();
+		job.setJobTitle("kahinaUpdate");
+		jobRepository.save(job);
+		
+		assertThat("kahinaUpdate").isEqualTo(jobRepository.findById("AC_ACCOUNT"));
+		//assertThat("kahinaUpdate").isEqualTo(jobRepository.findById("AC_ACCOUNT").getJobTitle());
 	}
+	
+	
 
 }
 
